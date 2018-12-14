16e1f56975863cf7e84fbefd899edf0b864a4e8a
philipplaeubli <phil@dreipol.ch>
2018-12-14 17:48:10 +0100

    fix nginx

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-envsubst '${SERVICE},${STATICFILES_LOCATION},${MEDIAFILES_LOCATION},${MINIO_HOST},${MINIO_PORT},${BUCKET_NAME}' < /nginx/nginx.template > /nginx/nginx.conf && exec nginx -g 'daemon off;'
+envsubst '${SERVICE},${STATICFILES_LOCATION},${MEDIAFILES_LOCATION}m${SECUREFILES_LOCATION},${MINIO_HOST},${MINIO_PORT},${BUCKET_NAME}' < /nginx/nginx.template > /nginx/nginx.conf && exec nginx -g 'daemon off;'
