9e85873097f3a977d66c2538f205040d7ea7eeb6
philipplaeubli <phil@dreipol.ch>
2018-12-14 13:15:24 +0100

    Fixes envsubst and formatting

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-envsubst < /nginx/nginx.template > /nginx/nginx.conf && exec nginx -g 'daemon off;'
+envsubst '${SERVICE},${STATICFILES_LOCATION},${MEDIAFILES_LOCATION},${MINIO_HOST},${MINIO_PORT},${BUCKET_NAME}' < /nginx/nginx.template > /nginx/nginx.conf && exec nginx -g 'daemon off;'
